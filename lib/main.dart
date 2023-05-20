d1600911910b2db4f3ae26a42f7ff2b16fbaff86
Mariam-Alboziri <102658576+Mariam-Alboziri@users.noreply.github.com>
2023-05-21 02:51:09 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold (
+        backgroundColor: Colors.white,
+        appBar: AppBar(title: Text('AROPE Syria'),
+        backgroundColor: Colors.blue[900],),
+       // body: Image(image: ),
+      ),
+     ),
+  );
+  
+}
