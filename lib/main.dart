42c3d38d2ffadca56a72f5a9dbbf8cba71f9c7bc
Mariam-Alboziri <102658576+Mariam-Alboziri@users.noreply.github.com>
2023-05-22 23:56:29 +0300

    first design

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,44 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold (
+      home: Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(title: Text('AROPE Syria'),
-        backgroundColor: Colors.blue[900],),
-       // body: Image(image: ),
+        body: SafeArea(
+          child: Center(
+            child: Column(children: [
+              Container(
+                width: 400.0,
+                child: Image(image: AssetImage('images/logoe.jpg')),
+              ),
+              Container(
+                child: Text(
+                  'Everything you need to know about your insurance',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text ('Get Started' ,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                     // fontWeight: FontWeight.bold,
+                      ),
+                  
+                  ),
+                ),
+                color:Color.fromARGB(255, 8, 48, 109) ,
+                width: 400.0,
+                height: 75.0,
+              ),
+            ]),
+          ),
+        ),
       ),
-     ),
+    ),
   );
-  
 }
